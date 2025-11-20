3ecd575daa9caccc2e873008a95da3029b0e56ff
Aayush Oza <aayushoza2804@gmail.com>
2025-11-21 00:43:59 +0530

    Update analytics_routes.py

--- routes/analytics_routes.py
+++ routes/analytics_routes.py
@@ -1,10 +1,13 @@
 from flask import Blueprint
 from models import db, OrderItem, Product
 from sqlalchemy import func   # 🔥 REQUIRED
+from flask_cors import cross_origin
+
 
 analytics_bp = Blueprint('analytics', __name__)
 
 @analytics_bp.get('/sales-by-color')
+@cross_origin()
 def sales_by_color():
     results = db.session.query(Product.color, func.sum(OrderItem.quantity)) \
         .join(Product, Product.id == OrderItem.product_id) \
@@ -13,6 +16,7 @@ def sales_by_color():
     return {color: qty for color, qty in results}
 
 @analytics_bp.get('/sales-by-design')
+@cross_origin()
 def sales_by_design():
     results = db.session.query(Product.design, func.sum(OrderItem.quantity)) \
         .join(Product, Product.id == OrderItem.product_id) \
@@ -21,6 +25,7 @@ def sales_by_design():
     return {design: qty for design, qty in results}
 
 @analytics_bp.get('/sales-by-cloth')
+@cross_origin()
 def sales_by_cloth():
     results = db.session.query(Product.cloth_type, func.sum(OrderItem.quantity)) \
         .join(Product, Product.id == OrderItem.product_id) \
