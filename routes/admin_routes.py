566b2694723461d484913bb51dde4dd0c91f10c0
Aayush Oza <aayushoza2804@gmail.com>
2025-11-21 00:44:46 +0530

    Update admin_routes.py

--- routes/admin_routes.py
+++ routes/admin_routes.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from models import User, Product, Order
+from flask_cors import cross_origin
 
 admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.get('/stats')
+@cross_origin()
 def stats():
     return {
         "users": User.query.count(),
         "products": Product.query.count(),
         "orders": Order.query.count()
     }
-
