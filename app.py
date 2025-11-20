6cf440d9ecacd3848a15999cc93d65c4a75d367b
Aayush Oza <aayushoza2804@gmail.com>
2025-11-20 21:51:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,7 @@ def create_app():
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
         return response, 204
 
-    @app.after_request
-    def add_cors_headers(response):
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        return response
+    
 
     @app.route("/")
     def home():
