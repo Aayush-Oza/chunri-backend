bdb35637ae8c09394ab23285786bfc1422cc679e
Aayush Oza <aayushoza2804@gmail.com>
2025-11-21 12:25:44 +0530

    Update product.py

--- models/product.py
+++ models/product.py
@@ -11,4 +11,6 @@ class Product(db.Model):
     size = db.Column(db.String(50))
     color = db.Column(db.String(50))
     image_url = db.Column(db.String(255))
-    active = db.Column(db.Boolean, default=True)
+
+    # SOFT DELETE FLAG
+    is_active = db.Column(db.Boolean, default=True)
