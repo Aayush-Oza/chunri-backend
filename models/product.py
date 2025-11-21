d0defb05d1b6c54d8dd58b4f12734338a8557adf
Aayush Oza <aayushoza2804@gmail.com>
2025-11-21 13:22:13 +0530

    Update product.py

--- models/product.py
+++ models/product.py
@@ -2,6 +2,7 @@ from . import db
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    product_no = db.Column(db.String(50), unique=True)   # <-- ADD THIS
     name = db.Column(db.String(120))
     description = db.Column(db.Text)
     price = db.Column(db.Float)
@@ -11,6 +12,4 @@ class Product(db.Model):
     size = db.Column(db.String(50))
     color = db.Column(db.String(50))
     image_url = db.Column(db.String(255))
-
-    # SOFT DELETE FLAG
     is_active = db.Column(db.Boolean, default=True)
